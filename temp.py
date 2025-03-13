b4515e22eaf2b45bc1d1d67972f3704bee6d637c
azizbeco <azizbektohirjonov06@gmail.com>
2025-03-13 21:24:10 +0500

    list and dict

--- temp.py
+++ temp.py
@@ -1,3 +1,65 @@
-print("Hello World")
+
+
+# name=input(">>>")
+
+# second_name=name[:str(name).find(" ")]
+
+# first_name=name[name.find(" ") :name.find(" ",name.find(" ")+1)]
+
+# last=name[name.find(" ",name.find(" ")+1):]
+
+# print(f"{second_name}\n{first_name}\n{last}")
+
+
+
+# a = input('Iltimos matematik amalni kiriting \n + - * /\n')
+
+# num1=int(input('>>>'))
+# num2=int(input('>>>'))
+
+# if a=='+':
+#     sum=num1+num2
+#     print(sum)
+
+# elif a=='-':
+#     sum=num1-num2
+#     print(sum)
+
+
+# elif a=='*':
+#     sum=num1*num2
+#     print(sum)
+
+# else:
+#     sum=num1/num2
+#     print(sum)
+
+
+# moths=['yanvar','fevral','mart','aprel','may','iyun','iyul','august','sentabr','octabr','noyabr','dekabr']
+
+# num=int(input('>>>'))
+
+# print(moths[num-1])
+    
+
+# del a ['tovar']
+
+# product={}
+
+# while True:
+#     a=input('>>>')
+    
+#     if a=='add':
+#         name=input('tavar nomi :')
+#         price=input('tavar narxi: ')
+#         product[name]=price
+#     elif a=='list':
+#        print(product)
+#     elif a=='delete':
+#         nom=input("O'chirmoqci bo'lgan tovar nomini kiriting\n>>>")
+#         del product [nom]
+#     else:
+#         print("Itimos tog'ri malumot kiriting \nadd| delete | list")
+
 
 
