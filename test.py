e9f3b3c88bbea57c12a082089291477f30e35c1f
azizbeco <azizbektohirjonov06@gmail.com>
2025-04-22 21:10:06 +0500

    Takrorlash

--- test.py
+++ test.py
@@ -1,108 +1,63 @@
 
-# class Animal:
-#     def speak(self):
-#         return "???"
-    
-# class Dog(Animal):
-#     def speak(self):
-#         return "Woof"
-# class cat(Animal):
-#     def  speak(self):
-#         return "Meow"
-
-# def animal_sound(animal):
-#     print(animal.speak())
-
-# animal_sound(Dog())
-
-# animal_sound(cat())
-
-# animal_sound(Animal())
 
 
-
-    
-# class Employer:
-#     def work(self):
-#         return " All "
-
-# class Manager(Employer):
-#     def work(self):
-#         return "I am Manager"
-    
-# class Accounter(Employer):
-#     def work(self):
-#         return "I am Accounter"
-#     def travel_time(self, distance):
-# class Worker(Employer):
-#     def work(self):
-#         return "I am worker"
+# d1={'x':2,'y':3}
+# d2={'x':3,'y':2}
 
 
-# def employer_method(employer):
-#     print(employer.work())
+# newdict = d1 | d2
 
 
-# employer_method(Manager())
-# employer_method(Accounter())
-# employer_method(Worker())
+# print(newdict)
 
+# a = {1,2,3}
+# b= {2,3,4}
 
+# newset = a|b
+# print(newset) # {1,2,3,4}
 
-# class Transport:
-#     def travel_time(self,distance):
-#         return distance/1
-    
-# class Bus(Transport):
-#     def travel_time(self, distance):
-#         return distance/40
-# class Bike(Transport):
-#     def travel_time(self, distance):
-#         return distance/60
-    
-# class Car(Transport):
+# newset = a & b
+# print(newset) # {2,3}
 
-#         return distance/100
-    
-# class Walk(Transport):
-#     def __init__(self):
-#         return 
-    
-# print(Bike().travel_time(1000))
-# print(Bike().travel_time(1000))
-# print(Car().travel_time(1000))
-# print(Walk().travel_time(1000))
+# newset = a - b 
+# print(newset) # {1,2,}
+
+# newset = a ^ b
+# print(newset) # {1,4}
+
+ 
 
 
 
 
-class Character:
-    def __init__(self,name,hp):
-        self.name = name
-        self.hp = hp
-    def attack(self):
-        return "Attack"
+# match command:
+#     case 'add':
+#         print('adding')
+#     case 'remove':
+#         print('removing')
+#     case 'update':
+#         print('updating ')
 
-    def take_damage(self,amount):
-        self.hp -= amount
-        print(f'{self.name} get {amount} damage. Left {self.hp} HP.')
-    def status(self):
-        print(f'{self.name} | { self.hp}')
-
-class Warrior(Character):
-    def attack(self):
-        return f'{self.name} attack with sword and take 20 damage'
+# if command=='add':
+#     print('Adding..')
+# elif command=='remove':
+#     print('removing..')
+# else:
+#     print('unkknown command')
 
 
 
-class Mage(Character):
-    def attack(self):
-        return f'{self.name} attack with Mage and take 30 damage'
-    
+
+try:
+    a = int(input('enter a number: '))
+    print(a**2)
+except:
+    print('siz raqam kiritmadingz!')
+finally:
+    print('Dastur tugadi')
 
 
-class Archer(Character):
-    def attack(self):
-        return f'{self.name} attack with Arcch and take 25 damage'
+
+
 
 
